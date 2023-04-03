4faf7298d91c0cf925204977586c4786f71dfe12
Nelly Kim <sk8high3r@ya.ru>
2023-04-03 23:29:31 +0300

    Add working solution (Arrays.replace function)

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,14 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        # We take first element of an array as base for comparing
+        max = array[0]
+
+        # Finding max element from the array
+        array.each { |el| max = el if el > max }
+
+        # Returning an array with replaced positive elements
+        array.map { |el| el.positive? ? max : el }
       end
 
       def search(_array, _query)
