535a8ec376974a0a1fa57885a46425c428ae3bed
Nelly Kim <sk8high3r@ya.ru>
2023-04-04 12:45:25 +0300

    Remove comments

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,23 +2,18 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        # We take first element of an array as base for comparing
         max = array[0]
 
-        # Finding max element from the array
         array.each { |el| max = el if el > max }
 
-        # Returning an array with replaced positive elements
         array.map { |el| el.positive? ? max : el }
       end
 
       def search(array, query)
-        # Getting indices of left and right elements
         low = 0
         high = array.size - 1
 
         while low <= high
-          # Getting element in the middle
           mid = low + ((high - low) / 2)
 
           if array[mid] < query
@@ -30,7 +25,6 @@ module Exercise
           return mid if array[mid] == query
         end
 
-        # Return -1 if element is not found
         -1
       end
     end
