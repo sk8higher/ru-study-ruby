203df5b040a1a4d140c9950e32197f41bec7a105
Nelly Kim <sk8high3r@ya.ru>
2023-04-06 00:45:10 +0300

    Add solution to rating task

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,16 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered_films = array.select { |film| film['country']&.include?(',') && film['rating_kinopoisk'].to_f.positive? }
+
+        films_ratings = filtered_films.map { |film| film['rating_kinopoisk'].to_f }
+
+        return 0 if films_ratings.empty?
+
+        total_rating = films_ratings.reduce(:+)
+
+        total_rating / films_ratings.size
       end
 
       def chars_count(_films, _threshold)
